07f074ce6107c43ab9640235c737beaffa3961b3
Vasquez Franco <franco.vasquez9897@gmail.com>
2026-03-14 20:48:44 -0300

    TOASTS para errores aun en fase de prueba

--- obj/Debug/net9.0/AlqoMishi.AssemblyInfo.cs
+++ obj/Debug/net9.0/AlqoMishi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlqoMishi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d8608496671ee6e09ff5b99aa98a33a0263a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1db35d4ceb25f35e39da011d39f41fb72667775e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlqoMishi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlqoMishi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
